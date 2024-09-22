34afcdef2b7097a6789f22fcf728971de750b7b6
정우혁 <alalalalalalall367@gmail.com>
2024-09-22 21:26:14 +0900

    feat : 회원가입을 위한 DTO 클래스 설계

---
+++ src/main/java/com/phu/backend/dto/member/request/SingInRequest.java
@@ -0,0 +1,32 @@
+package com.phu.backend.dto.member.request;
+
+import com.phu.backend.domain.member.Part;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
+@Schema(description = "회원이 회원가입할때 필요한 데이터")
+public class SingInRequest {
+    @NotBlank(message = "이메일을 입력하세요")
+    @Email(message = "이메일 형식이 아닙니다")
+    @Schema(description = "사용자 이메일", nullable = false, example = "mr6208@naver.com")
+    private String email;
+    @NotBlank(message = "비밀번호를 입력하세요")
+    @Schema(description = "사용자 비밀번호", nullable = false, example = "asdf1020")
+    private String password;
+    @NotBlank(message = "이름을 입력하세요")
+    @Schema(description = "사용자 이름", nullable = false, example = "정우혁")
+    private String name;
+    @NotNull(message = "나이를 입력하세요")
+    @Schema(description = "사용자 나이", nullable = false, example = "27")
+    private Integer age;
+    @NotBlank(message = "전화번호를 입력하세요")
+    @Schema(description = "사용자 전화번호", nullable = false, example = "01046666208")
+    private String tel;
+    @NotNull(message = "트레이너 혹은 회원인지 선택해주세요")
+    @Schema(description = "사용자 분류", nullable = false, example = "TRAINER")
+    private Part part;
+}
