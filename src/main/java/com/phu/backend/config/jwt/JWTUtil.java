bdf7937d0d2e5176c42715e16d0639a15012a705
정우혁 <alalalalalalall367@gmail.com>
2024-09-23 17:29:30 +0900

    feat : 토큰 발급을 위한 JWTUtil 설계

---
+++ src/main/java/com/phu/backend/config/jwt/JWTUtil.java
@@ -0,0 +1,62 @@
+package com.phu.backend.config.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+
+    private final SecretKey secretKey;
+    private final int accessTokenExpirationMinutes;
+    private final int refreshTokenExpirationMinutes;
+
+    public JWTUtil(@Value("${jwt.secret}") String secret,
+                   @Value("${jwt.access-token-expiration-minutes}") int accessTokenExpirationMinutes,
+                   @Value("${jwt.refresh-token-expiration-minutes}") int refreshTokenExpirationMinutes) {
+
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+        this.accessTokenExpirationMinutes = accessTokenExpirationMinutes;
+        this.refreshTokenExpirationMinutes = refreshTokenExpirationMinutes;
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("username", String.class);
+    }
+
+    public String getRole(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("role", String.class);
+    }
+
+    public String getCategory(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().get("category", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String category, String username, String role) {
+        int expiredMs = 0;
+        if (category.equals("access")) {
+            expiredMs = this.accessTokenExpirationMinutes;
+        }
+        if (category.equals("refresh")) {
+            expiredMs = this.refreshTokenExpirationMinutes;
+        }
+
+        return Jwts.builder()
+                .claim("category", category)
+                .claim("username", username)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(secretKey)
+                .compact();
+    }
+}
