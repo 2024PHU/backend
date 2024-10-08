7135a4f7247217447259acaae2ae526289346cd7
정우혁 <alalalalalalall367@gmail.com>
2024-10-08 19:29:30 +0900

    feat : MemberListRepository 내부 쿼리메서드 추가

--- src/main/java/com/phu/backend/repository/member/MemberListRepository.java
+++ src/main/java/com/phu/backend/repository/member/MemberListRepository.java
@@ -4,9 +4,12 @@ import com.phu.backend.domain.member.Member;
 import com.phu.backend.domain.member.MemberList;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 
 public interface MemberListRepository extends JpaRepository<MemberList, Long> {
     Optional<MemberList> findByTrainerAndMemberEmail(Member member, String email);
+
+    List<MemberList> findAllByTrainer(Member member);
 }
