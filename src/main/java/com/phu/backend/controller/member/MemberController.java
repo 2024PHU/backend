6baf0055392a255d8ed0c2b19f61d575d5698766
정우혁 <alalalalalalall367@gmail.com>
2024-10-08 18:12:58 +0900

    feat : 자신의 정보를 조회하는 API 및 트레이너가 회원을 추가하는 API 설계

--- src/main/java/com/phu/backend/controller/member/MemberController.java
+++ src/main/java/com/phu/backend/controller/member/MemberController.java
@@ -1,13 +1,17 @@
 package com.phu.backend.controller.member;
 
+import com.phu.backend.dto.member.request.AddMemberRequest;
 import com.phu.backend.dto.member.request.SignUpRequest;
 import com.phu.backend.dto.member.request.SignUpSocial;
+import com.phu.backend.dto.member.response.MemberResponse;
 import com.phu.backend.service.member.MemberService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,5 +33,17 @@ public class MemberController {
     @Operation(summary = "소셜 회원가입", description = "사용자가 소셜 로그인 회원가입을 한다")
     public void signUp(@RequestBody @Valid SignUpSocial request, HttpServletRequest servletRequest) {
         memberService.signUpSocial(request, servletRequest);
+    }
+
+    @PostMapping("/member/add")
+    @Operation(summary = "회원 추가", description = "트레이너가 자신의 회원을 추가한다.")
+    public void addMember(@RequestBody @Valid AddMemberRequest request) {
+        memberService.addMember(request);
+    }
+
+    @GetMapping("/member")
+    @Operation(summary = "자기정보 조회", description = "사용자가 자기 정보를 조회한다")
+    public ResponseEntity<MemberResponse> userInfo() {
+        return ResponseEntity.ok().body(memberService.userInfo());
     }
 }
