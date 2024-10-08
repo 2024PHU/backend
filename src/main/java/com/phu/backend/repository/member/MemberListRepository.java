342606f676f4fbc35f5be2e6bc1ad48f052265b8
정우혁 <alalalalalalall367@gmail.com>
2024-10-08 18:16:07 +0900

    feat : MemberListRepository 설계 및 쿼리메서드 작성

---
+++ src/main/java/com/phu/backend/repository/member/MemberListRepository.java
@@ -0,0 +1,12 @@
+package com.phu.backend.repository.member;
+
+import com.phu.backend.domain.member.Member;
+import com.phu.backend.domain.member.MemberList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+
+public interface MemberListRepository extends JpaRepository<MemberList, Long> {
+    Optional<MemberList> findByTrainerAndMemberEmail(Member member, String email);
+}
