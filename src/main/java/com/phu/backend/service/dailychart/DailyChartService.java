dde21d2ff6c32dda710f7995785213d1012f5956
정우혁 <alalalalalalall367@gmail.com>
2024-10-29 16:00:27 +0900

    feat : DailyChartService 내부 로직 설계

---
+++ src/main/java/com/phu/backend/service/dailychart/DailyChartService.java
@@ -0,0 +1,83 @@
+package com.phu.backend.service.dailychart;
+
+import com.phu.backend.domain.dailychart.Branch;
+import com.phu.backend.domain.dailychart.DailyChart;
+import com.phu.backend.domain.dailychart.ExerciseArea;
+import com.phu.backend.domain.dailychart.Routine;
+import com.phu.backend.domain.member.Member;
+import com.phu.backend.dto.dailychart.request.PtDailyChartRequest;
+import com.phu.backend.dto.dailychart.response.DailyChartResponse;
+import com.phu.backend.exception.dailychart.BranchIsNotValidException;
+import com.phu.backend.exception.dailychart.NotFoundChartException;
+import com.phu.backend.exception.member.NotFoundMemberException;
+import com.phu.backend.repository.dailychart.DailyChartRepository;
+import com.phu.backend.repository.dailychart.RoutineRepository;
+import com.phu.backend.repository.member.MemberRepository;
+import com.phu.backend.service.member.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class DailyChartService {
+    private final DailyChartRepository dailyChartRepository;
+    private final RoutineRepository routineRepository;
+    private final MemberRepository memberRepository;
+    private final MemberService memberService;
+
+    @Transactional
+    public Long addChartPt(PtDailyChartRequest request) {
+        if (request.getBranch().equals(Branch.PERSONAL)) {
+            throw new BranchIsNotValidException();
+        }
+        Member trainer = memberService.getMember();
+
+        Member member = memberRepository.findById(request.getId())
+                .orElseThrow(NotFoundMemberException::new);
+
+        DailyChart chart = DailyChart.builder()
+                .chartDate(request.getChartDate())
+                .trainer(trainer)
+                .memberEmail(member.getEmail())
+                .branch(request.getBranch())
+                .weight(request.getWeight())
+                .memo(request.getMemo())
+                .build();
+
+        dailyChartRepository.save(chart);
+
+        List<Routine> routines = request.getRoutines().stream()
+                .map(exerciseArea -> Routine.builder()
+                        .routine(exerciseArea)
+                        .dailyChart(chart)
+                        .build())
+                .collect(Collectors.toList());
+
+        routineRepository.saveAll(routines);
+
+        return chart.getId();
+    }
+
+    public DailyChartResponse getDailyCart(Long id) {
+        DailyChart dailyChart = dailyChartRepository.findById(id)
+                .orElseThrow(NotFoundChartException::new);
+
+        List<Routine> routines = routineRepository.findAllByDailyChart(dailyChart);
+
+        List<ExerciseArea> exerciseAreas = routines.stream()
+                .map(Routine::getRoutine).collect(Collectors.toList());
+
+        return DailyChartResponse.builder()
+                .id(dailyChart.getId())
+                .branch(dailyChart.getBranch())
+                .weight(dailyChart.getWeight())
+                .chartDate(dailyChart.getChartDate())
+                .routines(exerciseAreas)
+                .memo(dailyChart.getMemo())
+                .build();
+    }
+}
