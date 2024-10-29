b22276481b2f7df83f51b19e7ce1753174964d99
정우혁 <alalalalalalall367@gmail.com>
2024-10-29 15:57:28 +0900

    feat : DailyChart 도메인 설계

---
+++ src/main/java/com/phu/backend/domain/dailychart/DailyChart.java
@@ -0,0 +1,41 @@
+package com.phu.backend.domain.dailychart;
+
+import com.phu.backend.domain.member.Member;
+import com.phu.backend.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class DailyChart extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "daily_chart_id")
+    private Long id;
+    @Enumerated(value = EnumType.STRING)
+    private Branch branch;
+    private Integer weight;
+    private String memo;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member trainer;
+    private String memberEmail;
+    private LocalDate chartDate;
+//    @OneToMany(mappedBy = "DailyChart")
+//    private List<DailyChart> routines;
+        //TODO 사진추가
+    @Builder
+    public DailyChart(Branch branch, Integer weight, String memo, Member trainer, String memberEmail, LocalDate chartDate) {
+        this.branch = branch;
+        this.weight = weight;
+        this.memo = memo;
+        this.trainer = trainer;
+        this.memberEmail = memberEmail;
+        this.chartDate = chartDate;
+    }
+}
