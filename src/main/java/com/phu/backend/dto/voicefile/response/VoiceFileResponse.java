dd17b02e9a8fe47f825f758bb14508f8107d1a66
정우혁 <alalalalalalall367@gmail.com>
2024-11-12 17:55:56 +0900

    feat : VoiceFileResponse 내부 데이터 id 값 추가

--- src/main/java/com/phu/backend/dto/voicefile/response/VoiceFileResponse.java
+++ src/main/java/com/phu/backend/dto/voicefile/response/VoiceFileResponse.java
@@ -7,14 +7,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class VoiceFileResponse {
+    private Long id;
     private String originalFileName;
     private String uploadFileName;
     private String uploadFileUrl;
     private String message;
 
     @Builder
-
-    public VoiceFileResponse(String originalFileName, String uploadFileName, String uploadFileUrl, String message) {
+    public VoiceFileResponse(Long id, String originalFileName, String uploadFileName, String uploadFileUrl, String message) {
+        this.id = id;
         this.originalFileName = originalFileName;
         this.uploadFileName = uploadFileName;
         this.uploadFileUrl = uploadFileUrl;
