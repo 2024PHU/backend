ca506fab259a386a96ce3dbd7f251c6d54f4cdbb
정우혁 <alalalalalalall367@gmail.com>
2024-11-12 17:55:35 +0900

    feat : 음성녹음 저장소 내부 쿼리 메서드 추가

--- src/main/java/com/phu/backend/repository/voicefile/VoiceFileRepository.java
+++ src/main/java/com/phu/backend/repository/voicefile/VoiceFileRepository.java
@@ -1,7 +1,11 @@
 package com.phu.backend.repository.voicefile;
 
+import com.phu.backend.domain.member.Member;
 import com.phu.backend.domain.voicefile.VoiceFile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface VoiceFileRepository extends JpaRepository<VoiceFile, Long> {
+    List<VoiceFile> findAllByTrainer(Member member);
 }
