149dd6c5ca6598c4cca5b790230e0c1512785bac
정우혁 <alalalalalalall367@gmail.com>
2024-11-12 17:54:48 +0900

    feat : VoiceFileController 내부 음성파일 리스트 조회 API 설계

--- src/main/java/com/phu/backend/controller/voicefile/VoiceFileController.java
+++ src/main/java/com/phu/backend/controller/voicefile/VoiceFileController.java
@@ -1,14 +1,18 @@
 package com.phu.backend.controller.voicefile;
 
+import com.phu.backend.dto.voicefile.response.VoiceFileListResponse;
 import com.phu.backend.dto.voicefile.response.VoiceFileResponse;
 import com.phu.backend.service.voicefile.VoiceFileService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,5 +22,11 @@ public class VoiceFileController {
     @Operation(summary = "음성파일 업로드", description = "음성파일을 업로드한다")
     public ResponseEntity<VoiceFileResponse> uploadVoiceFile(@RequestPart(value = "file", required = false) MultipartFile multipartFile) {
         return ResponseEntity.ok().body(voiceFileService.uploadVoiceFile(multipartFile));
+    }
+
+    @GetMapping("/pt/voice-file")
+    @Operation(summary = "저장된 음성파일 리스트 조회", description = "저장된 음성파일 리스트를 전체조회한다.")
+    public ResponseEntity<List<VoiceFileListResponse>> getVoiceFileList() {
+        return ResponseEntity.ok().body(voiceFileService.getList());
     }
 }
