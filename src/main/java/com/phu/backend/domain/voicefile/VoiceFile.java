474bffb382ae4a14f4af2dda10047dd87f6eeead
정우혁 <alalalalalalall367@gmail.com>
2024-11-12 16:59:35 +0900

    feat : VoiceFile 을 핸들링하기 위한 VoiceFile 도메인 설계

---
+++ src/main/java/com/phu/backend/domain/voicefile/VoiceFile.java
@@ -0,0 +1,30 @@
+package com.phu.backend.domain.voicefile;
+
+import com.phu.backend.domain.member.Member;
+import com.phu.backend.global.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class VoiceFile extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "voice_file_id")
+    private Long id;
+    private String fileName;
+    private String uploadFileUrl;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member trainer;
+
+    @Builder
+    public VoiceFile(String fileName, String uploadFileUrl, Member trainer) {
+        this.fileName = fileName;
+        this.uploadFileUrl = uploadFileUrl;
+        this.trainer = trainer;
+    }
+}
