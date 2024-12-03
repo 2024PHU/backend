f2d8ae50e2c3ab8dd9f4fc447687b24d1b18996f
정우혁 <alalalalalalall367@gmail.com>
2024-12-03 14:43:06 +0900

    refactor: 회원 상세정보를 반환하는 MemberMemoResponseDTO 내부 전화번필드 추가

--- src/main/java/com/phu/backend/dto/member/response/MemberMemoResponse.java
+++ src/main/java/com/phu/backend/dto/member/response/MemberMemoResponse.java
@@ -7,22 +7,24 @@ import java.time.LocalDate;
 
 @Getter
 public class MemberMemoResponse {
-    private Long id;
+    private Long memberId;
     private String memberName;
     private Integer memberAge;
     private LocalDate ptStartDate;
     private LocalDate ptEndDate;
     private String memberTarget;
     private String significant;
+    private String tel;
 
     @Builder
-    public MemberMemoResponse(Long id, String memberName, Integer memberAge, LocalDate ptStartDate, LocalDate ptEndDate, String memberTarget, String significant) {
-        this.id = id;
+    public MemberMemoResponse(Long memberId, String memberName, Integer memberAge, LocalDate ptStartDate, LocalDate ptEndDate, String memberTarget, String significant, String tel) {
+        this.memberId = memberId;
         this.memberName = memberName;
         this.memberAge = memberAge;
         this.ptStartDate = ptStartDate;
         this.ptEndDate = ptEndDate;
         this.memberTarget = memberTarget;
         this.significant = significant;
+        this.tel = tel;
     }
 }
