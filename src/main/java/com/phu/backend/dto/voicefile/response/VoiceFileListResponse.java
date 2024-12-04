416fb4611d21b5c7bf66b93e655980c592118b13
정우혁 <alalalalalalall367@gmail.com>
2024-12-04 15:42:55 +0900

    feat: 음성파일 전체조회시 응답하는 VoiceFileListResponseDTO 내부 필드값 추가 및 필드명 변경

--- src/main/java/com/phu/backend/dto/voicefile/response/VoiceFileListResponse.java
+++ src/main/java/com/phu/backend/dto/voicefile/response/VoiceFileListResponse.java
@@ -11,23 +11,27 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 public class VoiceFileListResponse {
-    private Long id;
+    private Long fileId;
     private String uploadFileUrl;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
     private LocalDateTime createAt;
     private Long memberId;
+    private Boolean isTransformation;
 
     public VoiceFileListResponse(VoiceFile voiceFile) {
-        this.id = voiceFile.getId();
+        this.fileId = voiceFile.getId();
         this.uploadFileUrl = voiceFile.getUploadFileUrl();
         this.createAt = voiceFile.getCreatedAt();
         this.memberId = voiceFile.getMemberId();
+        this.isTransformation = voiceFile.getIsTransformation();
     }
+
     @Builder
-    public VoiceFileListResponse(Long id, String uploadFileUrl, LocalDateTime createAt, Long memberId) {
-        this.id = id;
+    public VoiceFileListResponse(Long fileId, String uploadFileUrl, LocalDateTime createAt, Long memberId, Boolean isTransformation) {
+        this.fileId = fileId;
         this.uploadFileUrl = uploadFileUrl;
         this.createAt = createAt;
         this.memberId = memberId;
+        this.isTransformation = isTransformation;
     }
 }
