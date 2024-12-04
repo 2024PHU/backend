58729fcea97fc22ad7c1ab513aa25107bc7fd1de
정우혁 <alalalalalalall367@gmail.com>
2024-12-04 15:45:19 +0900

    feat: ClovaSpeechResponseListDTO 내부 필드값 변경

--- src/main/java/com/phu/backend/dto/clova/ClovaSpeechResponseList.java
+++ src/main/java/com/phu/backend/dto/clova/ClovaSpeechResponseList.java
@@ -1,9 +1,11 @@
 package com.phu.backend.dto.clova;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,11 +13,16 @@ import java.util.UUID;
 @NoArgsConstructor
 public class ClovaSpeechResponseList {
     private UUID voiceListId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private LocalDateTime createAt;
+    private String memberName;
     private List<ClovaSpeechResponse> list;
 
     @Builder
-    public ClovaSpeechResponseList(UUID voiceListId, List<ClovaSpeechResponse> list) {
+    public ClovaSpeechResponseList(UUID voiceListId, LocalDateTime createAt, String memberName, List<ClovaSpeechResponse> list) {
         this.voiceListId = voiceListId;
+        this.createAt = createAt;
+        this.memberName = memberName;
         this.list = list;
     }
 }
