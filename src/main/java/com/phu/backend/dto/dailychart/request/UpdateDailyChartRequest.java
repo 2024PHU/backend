d1d2a85cd8b8ba474a39fbc71bec7f84b482b45e
정우혁 <alalalalalalall367@gmail.com>
2024-12-10 17:16:49 +0900

    feat: UpdateDailyChartRequestDTO 설계

--- src/main/java/com/phu/backend/dto/dailychart/request/UpdateDailyChartRequest.java
+++ src/main/java/com/phu/backend/dto/dailychart/request/UpdateDailyChartRequest.java
@@ -1,6 +1,5 @@
 package com.phu.backend.dto.dailychart.request;
 
-import com.phu.backend.domain.dailychart.Branch;
 import com.phu.backend.domain.dailychart.ExerciseArea;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
@@ -11,12 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Getter
-public class PtDailyChartRequest {
-    @NotNull(message = "차트를 추가할 회원의 아이디를 넣어주세요")
-    private Long id;
-    @NotNull(message = "차트 종류를 선택하세요")
-    @Schema(description = "차트 종류", nullable = false, example = "PT")
-    private Branch branch;
+public class UpdateDailyChartRequest {
     @NotNull(message = "운동 기록 날짜를 입력해주세요")
     @Schema(description = "운동 날짜", nullable = false, example = "2024-10-29")
     private LocalDate chartDate;
