98d0b5f215dbf3defb3abab5608fad3d0db927a2
정우혁 <alalalalalalall367@gmail.com>
2025-01-05 19:18:41 +0900

    feat: 기존 테스트 API 삭제 및 장문요약 API, 요약 조회 API 설계

--- src/main/java/com/phu/backend/controller/clova/ClovaController.java
+++ src/main/java/com/phu/backend/controller/clova/ClovaController.java
@@ -1,13 +1,17 @@
 package com.phu.backend.controller.clova;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.phu.backend.dto.clova.reqeust.ClovaMessage;
 import com.phu.backend.dto.clova.response.ClovaSpeechResponseList;
+import com.phu.backend.dto.summarization.response.SummarizationResponse;
 import com.phu.backend.service.clova.ClovaService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +25,15 @@ public class ClovaController {
         return ResponseEntity.ok().body(clovaService.clovaSpeech(fileId, memberId));
     }
 
-    @PostMapping("pt/clova/test")
-    @Operation(summary = "챗봇 테스팅", description = "테스트")
-    public ResponseEntity<?> test(@RequestBody ClovaMessage request) throws JsonProcessingException {
-        return ResponseEntity.ok(clovaService.test(request));
+    @GetMapping("pt/clova/summation/{voice-text-id}")
+    @Operation(summary = "장문요약", description = "분석한 텍스트를 요약해준다.")
+    public ResponseEntity<SummarizationResponse> summationTextFile(@PathVariable(name = "voice-text-id")UUID textId) throws JsonProcessingException {
+        return ResponseEntity.ok(clovaService.summationTextFile(textId));
+    }
+
+    @GetMapping("pt/clova/summation/get/{summarization-id}")
+    @Operation(summary = "요약된 텍스트 정보 조회", description = "요약된 텍스트 정보를 조회한다.")
+    public ResponseEntity<SummarizationResponse> getSummarization(@PathVariable(name = "summarization-id") Long summarizationId) {
+        return ResponseEntity.ok(clovaService.getSummarization(summarizationId));
     }
 }
